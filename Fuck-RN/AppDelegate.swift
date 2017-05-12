6050e30b93af26f9d991cf46bf2353b3b9938afd
xiaoP <anddygong@gmail.com>
2017-05-12 14:55:24 +0800

    初始化项目

---
+++ Fuck-RN/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fuck-RN
+//
+//  Created by xiaoP on 2017/5/12.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        
+        return true
+    }
+
+
+}
+
---
+++ Fuck-RN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fuck-RN
+//
+//  Created by xiaoP on 2017/5/12.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
